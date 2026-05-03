b54b433f1b7196faedd9169a09d4d859a817e405
angelochek887 <06799202a@gmail.com>
2026-05-03 23:15:06 +0200

    вв

--- lab 12/compile.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Program Files\Java\jdk-25.0.2\bin
-javac -d classes -sourcepath sources sources/ua/edu/sumdu/j2se/pr9/*.java
-pause
--- lab 12/run.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set CLASSPATH=.;classes;lib/*
-java -cp "%CLASSPATH%" ua.edu.sumdu.j2se.pr9.PhoneDriver db.properties
-pause
